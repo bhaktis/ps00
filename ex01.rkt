42865eb3dfb45a51267559e2ecdacb41c46bf8ef
bhaktis <sanglikar.b@husky.neu.edu>
2014-09-06 12:17:17 -0400

    ps00-ex01 solution

---
+++ ex01.rkt
@@ -0,0 +1,8 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex01) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+(* 60 (* 60 (* 366 24)))
+
+( * 366 24 60 60 )
+
+(* 60 (* 366 24 60))
