a112139300c50dc9457a964d05d3db00264deaf1
bhaktis <sanglikar.b@husky.neu.edu>
2014-09-06 14:52:43 -0400

    ps00-ex08 solution

---
+++ ex08.rkt
@@ -0,0 +1,16 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname ex08) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ())))
+; circle-area : Number -> Number
+; Given : Radius of the cirle 
+; Returns : Area of the circle given by the formula (pi * r^2)
+; Examples :
+; (circle-area 1) => 3.141592653589793
+; (circle-area 5) => 78.53981633974483
+; (circle-area 7) => 153.93804002589985
+
+(define (circle-area r)
+  (* pi (sqr r))
+  )
+
+
